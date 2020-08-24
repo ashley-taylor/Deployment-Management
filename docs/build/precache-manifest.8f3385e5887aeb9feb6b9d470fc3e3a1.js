07b451584acb59fee2f13f6c53160435e85724fa
Ashley Taylor <ashley.taylor@phocassoftware.com>
2020-08-24 13:53:47 +1200

    release to docs

---
+++ docs/build/precache-manifest.8f3385e5887aeb9feb6b9d470fc3e3a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185c9b87be3781f0c40406fabc4d887c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f466417f413ee4678f6",
+    "url": "/static/css/main.9fbe3e88.chunk.css"
+  },
+  {
+    "revision": "ca373058b0e412363e2f",
+    "url": "/static/js/2.3cbd9cdf.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.3cbd9cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f466417f413ee4678f6",
+    "url": "/static/js/main.c4d764fc.chunk.js"
+  },
+  {
+    "revision": "79f972f47b154b29f77d",
+    "url": "/static/js/runtime-main.52790aa4.js"
+  }
+]);
