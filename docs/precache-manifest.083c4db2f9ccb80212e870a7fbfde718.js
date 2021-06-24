6e358817ff086e2d6cc13254d4e7dbab2f4d878c
Ashley Taylor <ashley.taylor@phocassoftware.com>
2021-06-24 13:39:12 +1200

    fix ref error

---
+++ docs/precache-manifest.083c4db2f9ccb80212e870a7fbfde718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ffb6e3dbf391d5d5c7ecc2f0aa9076",
+    "url": "/Deployment-Management/index.html"
+  },
+  {
+    "revision": "542e161f2fd8767d5a16",
+    "url": "/Deployment-Management/static/css/main.9fbe3e88.chunk.css"
+  },
+  {
+    "revision": "5f11f057d15f2edd19ef",
+    "url": "/Deployment-Management/static/js/2.3b4ff675.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/Deployment-Management/static/js/2.3b4ff675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542e161f2fd8767d5a16",
+    "url": "/Deployment-Management/static/js/main.724a995d.chunk.js"
+  },
+  {
+    "revision": "25e910b542047ed44381",
+    "url": "/Deployment-Management/static/js/runtime-main.39d1c68d.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Deployment-Management/precache-manifest.5483cfd05c1e43f6423260995ee49056.js"
+  "/Deployment-Management/precache-manifest.083c4db2f9ccb80212e870a7fbfde718.js"
 );
 
 self.addEventListener('message', (event) => {
